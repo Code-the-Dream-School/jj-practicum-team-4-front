3b26555f5b23444c8b37d94a798f9e58c55ad32a
Uma Venki <iamumamsekar@gmail.com>
2025-08-29 17:20:08 -0700

    Added hero section for gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-import { Modal } from "@mui/material";
+import { Icon, Modal } from "@mui/material";
 import sampleImage from "../assets/images.jpeg";
+import hero from "../assets/hero4.png";
+import { CssBaseline } from "@mui/material";
 import UserCard from "../components/usercard/usercard.jsx";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import InsertDriveFileOutlinedIcon from "@mui/icons-material/InsertDriveFileOutlined";
+import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
+import ArrowRightAltOutlinedIcon from "@mui/icons-material/ArrowRightAltOutlined";
 import {
   Container,
   Grid,
@@ -10,6 +16,7 @@ import {
   CardContent,
   Typography,
   Box,
+  Stack,
 } from "@mui/material";
 
 export default function Gallery() {
@@ -49,59 +56,134 @@ export default function Gallery() {
   ]);
 
   return (
-    <Container maxWidth="lg" sx={{ py: 8 }}>
-      <Typography variant="h3" align="center" gutterBottom>
-        Gallery
-      </Typography>
-      <Grid container spacing={4} justifyContent="center">
-        {artworks.map((art) => (
-          <Grid item xs={12} sm={6} md={4} key={art.id}>
-            <Card onClick={() => setSelected(art)}>
-              <CardMedia
-                component="img"
-                height="200"
-                image={art.image}
-                alt={art.title}
-              />
-              <CardContent>
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Typography variant="h6">{art.title}</Typography>
-                  <Typography variant="body2" sx={{ ml: 2 }}>
-                    Likes: {art.likes}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      <Modal
-        open={!!selected}
-        aria-labelledby="modal-artwork-detail"
-        disableRestoreFocus
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box>
-          {selected && (
-            <UserCard
-              username={selected.user}
-              title={selected.title}
-              description={selected.description || ""}
-              image={selected.image}
-              isOpen={true}
-              onClose={() => setSelected(null)}
-            />
-          )}
+    <>
+      <CssBaseline />
+      <Container maxWidth="xl" disableGutters>
+        <Box
+          sx={{
+            backgroundImage: `url(${hero})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            minHeight: "35em",
+            position: "relative",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              textAlign: "center",
+              pb: 3,
+              width: "100%",
+              mx: 3,
+            }}
+          >
+            <Typography
+              variant="h4"
+              align="center"
+              sx={{ mb: 8 }}
+              margin-bottom="10px"
+            >
+              WEEKLY CHALLENGE TOPIC
+            </Typography>
+            <Typography variant="h5" align="center" sx={{ mb: 10 }}>
+              DURATION :
+            </Typography>
+            <Typography variant="h6" align="center" sx={{ mb: 10 }}>
+              INSTRUCTION/EXPLANATION:
+            </Typography>
+            <Stack
+              direction="row"
+              spacing={4}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box>
+                <AccountCircleOutlinedIcon fontSize="large" /> {/* your icon */}
+                <Typography align="center">
+                  Create
+                  <br />
+                  an account.
+                </Typography>
+              </Box>
+              <ArrowRightAltOutlinedIcon fontSize="large" />
+              <Box>
+                <FileUploadOutlinedIcon fontSize="large" />
+                {/* your icon */}
+                <Typography align="center">
+                  Upload your
+                  <br /> artwork image
+                </Typography>
+              </Box>
+              <ArrowRightAltOutlinedIcon fontSize="large" />
+              <Box>
+                <InsertDriveFileOutlinedIcon fontSize="large" />
+                <Typography align="center">
+                  Submit your
+                  <br />
+                  form!
+                </Typography>
+              </Box>
+            </Stack>
+          </Box>
         </Box>
-      </Modal>
-    </Container>
+      </Container>
+
+      <Container maxWidth="lg" sx={{ py: 8 }}>
+        <Typography variant="h3" align="center" gutterBottom>
+          Gallery
+        </Typography>
+        <Grid container spacing={4} justifyContent="center">
+          {artworks.map((art) => (
+            <Grid item xs={12} sm={6} md={4} key={art.id}>
+              <Card onClick={() => setSelected(art)}>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={art.image}
+                  alt={art.title}
+                />
+                <CardContent>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Typography variant="h6">{art.title}</Typography>
+                    <Typography variant="body2" sx={{ ml: 2 }}>
+                      Likes: {art.likes}
+                    </Typography>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Modal
+          open={!!selected}
+          aria-labelledby="modal-artwork-detail"
+          disableRestoreFocus
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box>
+            {selected && (
+              <UserCard
+                username={selected.user}
+                title={selected.title}
+                description={selected.description || ""}
+                image={selected.image}
+                isOpen={true}
+                onClose={() => setSelected(null)}
+              />
+            )}
+          </Box>
+        </Modal>
+      </Container>
+    </>
   );
 }
