e1f77c98bab752dd80fc3cfae10e3ed3a7858bb3
Uma Venki <iamumamsekar@gmail.com>
2025-09-07 18:17:53 -0700

    Container re-sizing

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -185,13 +185,37 @@ export default function Gallery() {
         </Box>
       </Container>
 
-      <Container maxWidth="lg" sx={{ py: 8 }}>
-        <Typography variant="h3" align="center" gutterBottom>
-          Gallery
-        </Typography>
-        <Grid container spacing={4} justifyContent="center">
+      <Container
+        maxWidth="xl"
+        sx={{
+          minHeight: "60vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          py: { xs: 6, md: 10 },
+        }}
+      >
+        <Box sx={{ width: "100%", mb: 4 }}>
+          <Typography variant="h3" align="center" gutterBottom>
+            Gallery
+          </Typography>
+        </Box>
+        <Grid
+          container
+          spacing={4}
+          justifyContent="center"
+          sx={{ width: "100%" }}
+        >
           {artworks.map((art) => (
-            <Grid item xs={12} sm={6} md={4} key={art.id}>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              key={art.id}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
               <Card onClick={() => setSelected(art)}>
                 <CardMedia
                   component="img"
